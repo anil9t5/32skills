61bfc80f88823d0861d48ba907cf654dff248d82
Anil Hitang <anilhitang@MacBook-Pro.local>
2025-04-16 14:34:00 -0300

    Home screen ui update

--- src/app/components/navbar/Navbar.js
+++ src/app/components/navbar/Navbar.js
@@ -14,24 +14,22 @@ const navItems = {
 
 export function Navbar() {
   return (
-    <aside>
-      <div>
-        <nav id="nav">
-          <div>
-            {Object.entries(navItems).map(([path, { name }]) => {
-              return (
-                <Link
-                  key={path}
-                  href={path}
-                  className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
-                >
-                  {name}
-                </Link>
-              );
-            })}
-          </div>
-        </nav>
-      </div>
-    </aside>
+    <div>
+      <nav id="nav">
+        <div>
+          {Object.entries(navItems).map(([path, { name }]) => {
+            return (
+              <Link
+                key={path}
+                href={path}
+                className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
+              >
+                {name}
+              </Link>
+            );
+          })}
+        </div>
+      </nav>
+    </div>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,28 @@
 import Image from "next/image";
+import "./globals.css";
 import styles from "./page.module.css";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main>
+      <header>
         <h1>anilhitang</h1>
         <p>Try out some random fun stuff</p>
+      </header>
+      <main>
+        <div className="grid-container">
+          <div className={styles.item_container}>
+            <Link href="/pages/jokes">Jokes</Link>
+          </div>
+          <div className={styles.item_container}>
+            <Link href="/pages/lyrics">Lyrics</Link>
+          </div>
+          <div className={styles.item_container}>
+            <Link href="/pages/lyrics">Orange Catcher</Link>
+          </div>
+        </div>
       </main>
-      <Link href="/pages/jokes">Jokes</Link>
-      <Link href="/pages/lyrics">Lyrics</Link>
     </div>
   );
 }

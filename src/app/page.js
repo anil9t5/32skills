3221cf03cfb4946262b65b742d534dd8b7c5072e
Anil Hitang <anilhitang@macbook-pro.home>
2025-04-28 17:27:20 -0300

    UI updates

--- src/app/page.js
+++ src/app/page.js
@@ -11,22 +11,42 @@ export default function Home() {
         <p>Try out some random fun stuff</p>
       </header>
       <div className="container">
+        <br />
+        <br />
         <div className="flex-container text-align-center">
           <div className={`${styles.item_container} flex-item`}>
             <Link href="/pages/jokes">
               <Image
                 src="/jokes.png"
-                width={400}
-                height={250}
+                width={600}
+                height={300}
                 alt="Picture of the menu"
               />
+              <h4>Jokes</h4>
             </Link>
           </div>
           <div className={`${styles.item_container} flex-item`}>
-            <Link href="/pages/lyrics">Lyrics</Link>
+            <Link href="/pages/lyrics">
+              {" "}
+              <Image
+                src="/lyrics-cover.png"
+                width={600}
+                height={300}
+                alt="Picture of the menu"
+              />
+              <h4>Lyrics Finder</h4>
+            </Link>
           </div>
           <div className={`${styles.item_container} flex-item`}>
-            <Link href="/pages/lyrics">Orange Catcher</Link>
+            <Link href="/pages/lyrics">
+              <Image
+                src="/orange-cover.png"
+                width={600}
+                height={300}
+                alt="Picture of the menu"
+              />
+              <h4>Orange Catcher</h4>
+            </Link>
           </div>
         </div>
       </div>

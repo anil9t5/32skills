6902f34d1ef91b541badc96755567de15620b66c
Anil Hitang <anilhitang@MacBook-Pro.local>
2025-04-29 10:24:51 -0300

    Added typewriter feature

---
+++ src/app/components/typewriters/Typewriter.js
@@ -0,0 +1,13 @@
+"use client";
+
+import SpeedTypingGame from "./SpeedTypingGame";
+
+function Typewriter() {
+  return (
+    <div id="typewriter">
+      <SpeedTypingGame />
+    </div>
+  );
+}
+
+export default Typewriter;
---
+++ src/app/pages/typewriters/page.js
@@ -0,0 +1,18 @@
+// import Joke from "@/app/components/jokes/Joke";
+import Typewriter from "@/app/components/typewriters/Typewriter";
+import React from "react";
+
+const Draws = () => {
+  return (
+    <div className="text-align-center">
+      <br />
+      <br />
+      <h2>Type</h2>
+      <br />
+      <br />
+      <Typewriter />
+    </div>
+  );
+};
+
+export default Draws;

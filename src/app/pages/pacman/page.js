09942715de157eedd53cd45181a204921058c31d
Anil Hitang <anilhitang@MacBook-Pro.local>
2025-04-29 15:50:34 -0300

    Work in progress for pacman feature

---
+++ src/app/pages/pacman/page.js
@@ -0,0 +1,17 @@
+import PacMan from "@/app/components/pacman/PacMan";
+import React from "react";
+
+const Pacman = () => {
+  return (
+    <div className="text-align-center">
+      <br />
+      <br />
+      <h2>Pacman</h2>
+      <br />
+      <br />
+      <PacMan />
+    </div>
+  );
+};
+
+export default Pacman;
